31c297141074e249ef74e27772671080ff5e9e07
Andrew Masih <armasih@outlook.com>
2021-05-30 17:42:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+/* example of indexing a multiDimentional Array */
+
+let multiDimentionalArray = [1, 2, 3, 4,[5, 6,[7, 8,[9, 10, 11, 12]]]];
+console.log(multiDimentionalArray);
+
+let dimensionalArray = multiDimentionalArray [4][2][2][0];
+
+console.log(dimensionalArray);
